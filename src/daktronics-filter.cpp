803b52b02ea73aea68e6afb135647e3b7106340f
Bjorn Peterson <76683417+bkpeterson@users.noreply.github.com>
2026-01-05 22:01:17 -0600

    Test

--- src/daktronics-filter.cpp
+++ src/daktronics-filter.cpp
@@ -44,7 +44,7 @@ void *DAKFilter::Create(obs_data_t *settings, obs_source_t *source)
 
 void DAKFilter::Destroy(void *data)
 {
-	auto instance = static_cast<DAKFilter *>(data);
+	//auto instance = static_cast<DAKFilter *>(data);
 	//DAKDataUtils::RemoveFilter(instance);
 }
 
